89726a088c958b8d351b85fad6a2c67d81116878
Rahul Sachwani <119300258+rahul101996@users.noreply.github.com>
2024-12-22 15:38:36 +0530

    navbar of the website is done

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sundown</title>
+    <link rel="stylesheet" href="/style.css">
+    <link rel="shortcut icon" href="/icon.png" type="image/x-icon">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body>
+    <div id="main" class="min-h-[100vh] bg-[#efeae3]">
+        <div class="flex items-center justify-center px-8 py-8">
+            <nav class="flex items-center justify-between w-full">
+                <img src="https://uploads-ssl.webflow.com/64d3dd9edfb41666c35b15b7/64d3dd9edfb41666c35b15c2_Sundown%20logo.svg" alt="">
+                <div class="flex items-center justify-center gap-4 relative ">
+                    <a href="#" class="relative navbutton group overflow-hidden rounded-full">
+                        <button class="text-lg font-[500] border border-[#b7b3ae] rounded-full px-5 py-2 z-10 relative group-hover:text-white overflow-hidden  group-hover:border-black">Work</button>
+                    </a>
+                    <a href="#" class="relative navbutton group overflow-hidden rounded-full">
+                        <button class="text-lg font-[500] border border-[#b7b3ae] rounded-full px-5 py-2 z-10 relative group-hover:text-white overflow-hidden group-hover:border-black">Studeo</button>
+                    </a> <a href="#" class="relative navbutton group overflow-hidden rounded-full">
+                        <button class="text-lg font-[500] border border-[#b7b3ae] rounded-full px-5 py-2 z-10 relative group-hover:text-white overflow-hidden group-hover:border-black">Contact</button>
+                    </a>
+                </div>
+            </nav>
+
+        </div>
+
+    </div>
+</body>
+
+</html>
